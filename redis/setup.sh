2710596c815d520336e35cffa63c4541c290cb50
Mikulas Vesely <mikface@gmail.com>
2022-12-08 20:38:34 -0500

    fix redis script

--- redis/setup.sh
+++ redis/setup.sh
@@ -9,7 +9,7 @@ yes 'yes' | redis-cli --cluster fix localhost:6379
 for x in {a..f}
 do
   echo "[INFO] Running Redis benchmark for workload $x..."
-  ./bin/ycsb.sh load redis -s -P "workloads/workload$x" -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.cluster=true" > "outputLoad$x"
-  ./bin/ycsb.sh run redis -s -P "workloads/workload$x" -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.cluster=true" > "outputRun$x"
+  bin/ycsb.sh load redis -s -P "workloads/workload$x" -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.cluster=true" > "../outputLoad$x"
+  bin/ycsb.sh run redis -s -P "workloads/workload$x" -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.cluster=true" > "../outputRun$x"
 done
 echo "[INFO] Done Redis benchmark."
